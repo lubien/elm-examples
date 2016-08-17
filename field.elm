97f75b4d33590d4e3a843e67499be59908a34e41
lubien <lubien1996@gmail.com>
2016-08-17 01:28:53 -0300

    SAVEPOINT

---
+++ field.elm
@@ -0,0 +1,30 @@
+import Html exposing (text, div, input)
+import Html.App exposing (beginnerProgram)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import String
+
+
+main =
+  beginnerProgram { model = "", view = view, update = update }
+
+
+type Msg = NewContent String
+
+update (NewContent content) oldContent =
+  content
+
+
+view content =
+  div []
+    [ input [ placeholder "Reverse me", onInput NewContent, myStyle] []
+    , div [] [ text (String.reverse content) ]
+    ]
+
+
+myStyle =
+  style
+    [ ("width", "100%")
+    , ("padding", "10px 0")
+    ]
+
---
+++ unordered-list.elm
@@ -0,0 +1,20 @@
+import Html exposing (li, text, ul)
+import Html.Attributes exposing (class)
+
+
+main =
+  ul [class "my-class"]
+    [ li [] [text "Bar"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    , li [] [text "Foo"]
+    ]
